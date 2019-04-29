3cfd1da50542c7f465b255249c72ef8fb180fab5
SwordmanYao <swordsman.yao@foxmail.com>
2019-04-29 17:10:59 +0800

    缩放

--- src/RxExampleComponent.tsx
+++ src/RxExampleComponent.tsx
@@ -15,26 +15,27 @@ export default function RxExampleComponent(props: IProps) {
   const [room, setRoom] = useState(1);
   const [offsetX, setOffsetX] = useState(0);
   const [offsetY, setOffsetY] = useState(0);
-  
-  
-  
+
   const containerRef = useRef<HTMLDivElement>(null);
 
   useLayoutEffect(() => {
+    // 拖拽
     if (nodeRef.current && containerRef.current) {
       const mouseDown$ = Rx.fromEvent<MouseEvent>(nodeRef.current, 'mousedown');
       const mouseMove$ = Rx.fromEvent<MouseEvent>(containerRef.current, 'mousemove');
       const mouseUp$ = Rx.fromEvent<MouseEvent>(document, 'mouseup');
-
       const nodePos$ = mouseDown$.pipe(
         switchMap(event => {
           return mouseMove$.pipe(
             map(moveEvent => {
-              console.log(moveEvent, moveEvent.clientY, moveEvent.pageY, event.offsetX, event.offsetY);
-              return {
-                x: moveEvent.pageX - event.offsetX,
-                y: moveEvent.pageY - event.offsetY,
-              };
+              if (containerRef.current) {
+                const { top, left } = containerRef.current.getBoundingClientRect();
+                return {
+                  x: moveEvent.clientX - left - event.offsetX, // moveEvent.clientX -- 鼠标相对可视区域距离; left -- 画布相对可是区域位置; event.offsetX -- 鼠标相对方块的位置
+                  y: moveEvent.clientY - top - event.offsetY,
+                };
+              }
+              return { x: 0, y: 0 };
             }),
             takeUntil(mouseUp$),
           );
@@ -43,49 +44,66 @@ export default function RxExampleComponent(props: IProps) {
 
       nodePos$.subscribe(posxy => {
         setPos(posxy);
+        console.log(posxy, 'posxy');
       });
     }
 
+    // 缩放
     if (containerRef.current) {
       const mouseWheel$ = Rx.fromEvent<WheelEvent>(containerRef.current, 'mousewheel');
       const room$ = mouseWheel$.pipe(
-        // map(event => ({
-        //   room: event.deltaY / 53,
-        //   offsetX: event.offsetX,
-        //   offset
-        // }),
-        scan<WheelEvent, { room: number, offsetX: number, offsetY: number }>((roomConf, event) => {
-          console.log(event, 'mousewheel');
-          const currentRoom = roomConf.room + event.deltaY * 0.1 / 53;
-          return {
-            room: currentRoom,
-            offsetX: roomConf.offsetX - event.offsetX * (currentRoom - roomConf.room),
-            offsetY: roomConf.offsetY - event.offsetY * (currentRoom - roomConf.room),
-          };
-        }, {
-          room: 1,
-          offsetX: 0,
-          offsetY: 0,
-        }),
+        scan<WheelEvent, { room: number; offsetX: number; offsetY: number }>(
+          (roomConf, event) => {
+            console.log(event, 'mousewheel');
+            const currentRoom = roomConf.room + (event.deltaY * 0.01) / 53;
+            if (containerRef.current) {
+              const boundingRect = containerRef.current.getBoundingClientRect();
+
+              const diffWidth = boundingRect.width * (currentRoom - roomConf.room);
+              const diffHeight = boundingRect.height * (currentRoom - roomConf.room);
+
+              // 鼠标相对画布的位置
+              const clientX = event.clientX - boundingRect.left;
+              const clientY = event.clientY - boundingRect.top;
+
+              const xFactor = (clientX - roomConf.offsetX) / roomConf.room / boundingRect.width;
+              const yFactor = (clientY - roomConf.offsetY) / roomConf.room / boundingRect.height;
+
+              console.log(roomConf, boundingRect, currentRoom, diffWidth, diffHeight, clientX, clientY);
+              return {
+                room: currentRoom,
+                offsetX: roomConf.offsetX - diffWidth * xFactor,
+                offsetY: roomConf.offsetY - diffHeight * yFactor,
+              };
+            }
+            return {
+              room: 1,
+              offsetX: 0,
+              offsetY: 0,
+            };
+          },
+          {
+            room: 1,
+            offsetX: 0,
+            offsetY: 0,
+          },
+        ),
       );
-      
+
       room$.subscribe(x => {
         setRoom(x.room);
         setOffsetX(x.offsetX);
         setOffsetY(x.offsetY);
       });
-      
-      mouseWheel$.subscribe(event => {
-        console.log(event, 'mousewheel');
-      });
     }
-
-
   }, [nodeRef.current]);
 
   return (
     <div ref={containerRef} className="container">
-      <div className="node-container" style={{ transform: `translate(${offsetX}px, ${offsetY}px) scale(${room})` }}>
+      <div
+        className="node-container"
+        style={{ transform: `translate(${offsetX}px, ${offsetY}px) scale(${room})` }}
+      >
         <div ref={nodeRef} style={{ left: pos.x, top: pos.y }} className="node">
           node
         </div>
